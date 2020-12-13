17a3ca42053638d48d84e4a6b2ae3d566f773321
julien210 <57074108+julien210@users.noreply.github.com>
2020-12-13 20:37:44 +0100

    typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Content } from "../components/content"
+import  Content  from "../components/content"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Scene from  "../components/scene"
@@ -7,7 +7,7 @@ import Scene from  "../components/scene"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    < Content />
+    <Content />
     <Scene />
   </Layout>
 )
